7c60ba8990036d4a353503ff4352e2ea22746f03
mic-warrior <mic-warrior@yandex.ru>
2020-02-18 23:46:05 +0400

    no message

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { ContentComponent } from './components/content/content.component';
+import { SearchPanelComponent } from './components/search-panel/search-panel.component';
+import { ItemsListComponent } from './components/items-list/items-list.component';
+import { ItemComponent } from './components/item/item.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    BreadcrumbsComponent,
+    ContentComponent,
+    SearchPanelComponent,
+    ItemsListComponent,
+    ItemComponent
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
