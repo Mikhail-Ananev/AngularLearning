27f15e0b667e5801c96ae0c11e33417c12cf38e6
mic-warrior <mic-warrior@yandex.ru>
2020-02-19 23:58:52 +0400

    Restore after unsuccessful commit

---
+++ src/app/core/models/interfaces.ts
@@ -0,0 +1,13 @@
+export interface CourseInfo {
+    id: Number;
+    title: String;
+    creationDate: Date;
+    duration: Number;
+    description: String;
+}
+
+export interface User {
+    id: Number;
+    firstName: String;
+    lastName: String;
+}
