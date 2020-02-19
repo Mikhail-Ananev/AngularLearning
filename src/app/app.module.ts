47d4188ea418cff6b72f0a1d3e44cd784b27084d
mic-warrior <mic-warrior@yandex.ru>
2020-02-20 01:03:57 +0400

    Installation FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { CoreModule } from './core/core.module';
     AppComponent
   ],
   imports: [
+    FontAwesomeModule,
     BrowserModule,
     AppRoutingModule,
     CoreModule
