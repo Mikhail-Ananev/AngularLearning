fef8a6ac876543c16aefd4f449619ccb9c4c1175
Mikhail_Ananev <Mikhail_Ananev@epam.com>
2020-03-07 14:38:26 +0400

    Change tests for dump components to class tests.

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,27 +1,10 @@
-import { Component } from '@angular/core';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeaderComponent, FaIconComponent ],
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
+    const component = new HeaderComponent();
+    component.ngOnInit();
+
     expect(component).toBeTruthy();
   });
 });
--- src/app/components/main/main.component.spec.ts
+++ src/app/components/main/main.component.spec.ts
@@ -1,61 +1,10 @@
-import { Component } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainComponent } from './main.component';
 
-@Component({
-  selector: 'app-header',
-  template: ''
-})
-class MockHeaderComponent {
-}
-
-@Component({
-  selector: 'app-breadcrumbs',
-  template: ''
-})
-class MockBreadcrumbsComponent {
-}
-
-@Component({
-  // tslint:disable-next-line: component-selector
-  selector: 'router-outlet',
-  template: ''
-})
-class MocRouterOutletComponent {
-}
-
-@Component({
-  selector: 'app-footer',
-  template: ''
-})
-class MockFooterComponent {
-}
-
 describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        MainComponent,
-        MockHeaderComponent,
-        MockBreadcrumbsComponent,
-        MocRouterOutletComponent,
-        MockFooterComponent
-      ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
+    const component = new MainComponent();
+    component.ngOnInit();
+
     expect(component).toBeTruthy();
   });
 });
