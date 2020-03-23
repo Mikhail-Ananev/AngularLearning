658ea776845df5b4470246341edbeb13f838ec3d
mic-warrior <mic-warrior@yandex.ru>
2020-03-23 10:01:36 +0400

    Add authentication logic

--- src/app/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./breadcrumbs.component.scss']
 })
 export class BreadcrumbsComponent implements OnInit {
+  public isAuthenticated: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   public ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
   }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/interfaces';
 import { faSignOutAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, User {
-  public id: number;
-  public firstName: string;
-  public lastName: string;
+export class HeaderComponent implements OnInit {
+  public isAuthenticated: boolean;
   public faSignOutAlt = faSignOutAlt;
   public faUser = faUser;
-
-  constructor() { }
+  
+  constructor(private authService: AuthService) { }
 
   public ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+  }
+
+  public logOff() {
+    this.authService.logout();
   }
 }
